5dcf71ba0f92782f06124ccc6436cd6210b2c1c0
YaRo <roman.yatsenko@hneu.net>
2020-04-11 17:01:14 +0300

    Add settings import

--- aliens.py
+++ aliens.py
@@ -7,7 +7,9 @@ from settings import Settings
 def run_game():
     # Инициализирует игру и создает объект экрана
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    game_settings = Settings()
+    screen = pygame.display.set_mode(
+        (game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Aliens")
     # Назначение цвета фона
     bg_color = (230, 230, 230)
@@ -19,7 +21,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # При каждом проходе цикла перерисовывается экран
-        screen.fill(bg_color)
+        screen.fill(game_settings.bg_color)
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
 
