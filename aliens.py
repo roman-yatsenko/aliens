e2d6275cb51bf36fa40f0921ac672bbc923da1d2
YaRo <roman.yatsenko@hneu.net>
2020-04-18 18:36:01 +0300

    Add gf.update_bullets

--- aliens.py
+++ aliens.py
@@ -24,14 +24,7 @@ def run_game():
         # Отслеживание событий клавиаутры и мыши
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-
-        # Удаление пуль вышедших за край экрана
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        # print(len(bullets))
-        
+        gf.update_bullets(bullets)
         gf.update_screen(game_settings, screen, ship, bullets)
 
 run_game()
