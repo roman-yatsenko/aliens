917ece5b050ba777c3ad09136c1fcbd56fda90d4
YaRo <roman.yatsenko@hneu.net>
2020-04-25 17:05:48 +0300

    Add fleet rows

--- game_functions.py
+++ game_functions.py
@@ -67,12 +67,19 @@ def get_number_aliens_x(game_settings, alien_width):
     number_alien_x = int(available_space_x / (2 * alien_width))
     return number_alien_x
 
-def create_alien(game_settings, screen, aliens, alien_number):
+def get_number_rows(game_settings, ship_height, alien_height):
+    """ Определяет количество рядов, помещающихся на экране"""
+    available_space_y = (game_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+def create_alien(game_settings, screen, aliens, alien_number, row_number):
     """ Создает пришельца и размещает его в ряду"""
     alien = Alien(game_settings, screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
 def create_fleet(game_settings, screen, aliens):
@@ -82,8 +89,10 @@ def create_fleet(game_settings, screen, aliens):
 
     alien = Alien(game_settings, screen)
     number_aliens_x = get_number_aliens_x(game_settings, alien.rect.width)
+    number_rows = get_number_rows(game_settings, ship.rect.height, alien.rect.height)
 
     # Создание первого ряда пришельцев
-    for alien_number in range(number_aliens_x):
-        # Создание пришельца и размещеение его в ряду
-        create_alien(game_settings, screen, aliens, alien_number)        
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            # Создание пришельца и размещеение его в ряду
+            create_alien(game_settings, screen, aliens, alien_number)        
