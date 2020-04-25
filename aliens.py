cbea56f83e1b8d67bc8079764fc36ed52087c852
YaRo <roman.yatsenko@hneu.net>
2020-04-25 18:50:07 +0300

    Add stats to run_game

--- aliens.py
+++ aliens.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -12,6 +13,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Aliens")
+
+    # Создание экземпляра для хранения игровой статистики
+    stats = GameStats(game_settings)
 
     # Создание корабля
     ship = Ship(game_settings, screen)
@@ -31,7 +35,7 @@ def run_game():
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(game_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(game_settings, ship, aliens)
+        gf.update_aliens(game_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(game_settings, screen, ship, aliens, bullets)
 
 run_game()
