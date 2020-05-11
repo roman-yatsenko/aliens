1e77973b631211eae7620ff393c7057fcad109f1
YaRo <roman.yatsenko@hneu.net>
2020-05-11 16:26:52 +0300

    Add game_active to run_game

--- aliens.py
+++ aliens.py
@@ -33,9 +33,12 @@ def run_game():
     while True:
         # Отслеживание событий клавиаутры и мыши
         gf.check_events(game_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(game_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(game_settings, stats, screen, ship, aliens, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(game_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(game_settings, stats, screen, ship, aliens, bullets)
+            
         gf.update_screen(game_settings, screen, ship, aliens, bullets)
 
 run_game()
