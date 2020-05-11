f35db2fe027d2f7472564af1101b7cd4bfb40947
YaRo <roman.yatsenko@hneu.net>
2020-05-11 16:52:59 +0300

    Add prep_msg

--- button.py
+++ button.py
@@ -19,4 +19,9 @@ class Button():
 
         # Сообщение кнопки
         self.prep_msg(msg)
-        
+    
+    def prep_msg(self, msg):
+        """Преобразует msg в прямоугольник и выравнивает текст по центру"""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
