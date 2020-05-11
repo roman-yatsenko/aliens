451b60d9d417257b731158f349049dead399ce84
YaRo <roman.yatsenko@hneu.net>
2020-05-11 17:10:20 +0300

    Add check_play_button

--- game_functions.py
+++ game_functions.py
@@ -40,6 +40,14 @@ def check_events(game_settings, screen, ship, bullets):
             check_keydown_events(event, game_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+def check_play_button(self, stats, play_button, mouse_x, mouse_y):
+    """Запускает новую игру при нажатии кнопки Play"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 def update_screen(game_settings, screen, stats, ship, aliens, bullets, play_button):
     """Обновляет изображения на экране и отображает новый экран"""
