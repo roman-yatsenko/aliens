c8371b2ccd6d8f819c2af5783f16e075f5bd5771
YaRo <roman.yatsenko@hneu.net>
2020-05-16 16:58:58 +0300

    Add sb to run_game

--- aliens.py
+++ aliens.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 import game_functions as gf
@@ -20,6 +21,7 @@ def run_game():
 
     # Создание экземпляра для хранения игровой статистики
     stats = GameStats(game_settings)
+    sb = Scoreboard(game_settings, screen, stats)
 
     # Создание корабля
     ship = Ship(game_settings, screen)
@@ -43,6 +45,6 @@ def run_game():
             gf.update_bullets(game_settings, screen, ship, aliens, bullets)
             gf.update_aliens(game_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(game_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(game_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
